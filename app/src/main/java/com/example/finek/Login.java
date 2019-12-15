7b31e52631e67b27d684c05f2fba875788c7d946
Dev5Services <services.dev5@gmail.com>
2019-12-16 00:28:12 +0100

    version 4

--- app/src/main/java/com/example/finek/Login.java
+++ app/src/main/java/com/example/finek/Login.java
@@ -2,13 +2,25 @@ package com.example.finek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
+    TextView textView1 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        textView1 = findViewById(R.id.inscription);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Inscription.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
