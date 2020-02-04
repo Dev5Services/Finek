13740e9ccdce403b56a8a4d687fee28671902fae
Abdo <abdelatif.azdoud@gmail.com>
2020-02-04 20:56:36 +0100

    zoning+localisation

---
+++ app/src/main/java/com/example/finek/MyFirebaseMessagingService.java
@@ -0,0 +1,66 @@
+package com.example.finek;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by NgocTri on 8/9/2016.
+ */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "Finek: Notification";
+
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        //FirebaseMessaging.getInstance().subscribeToTopic("lala");
+
+        Log.d(TAG, "FROM:" + remoteMessage.getFrom());
+
+        //Check if the message contains data
+        if(remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data: " + remoteMessage.getData());
+        }
+
+        //Check if the message contains notification
+
+        if(remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Mesage body:" + remoteMessage.getNotification().getBody());
+            sendNotification(remoteMessage.getNotification().getBody());
+        }
+    }
+
+    /**
+     * Dispay the notification
+     * @param body
+     */
+    private void sendNotification(String body) {
+
+        Intent intent = new Intent(this, LastLocation.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0/*Request code*/, intent, PendingIntent.FLAG_ONE_SHOT);
+        //Set sound of notification
+        Uri notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notifiBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Finek: Notification")
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setSound(notificationSound)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0 /*ID of notification*/, notifiBuilder.build());
+    }
+}
--- app/src/main/java/com/example/finek/Register.java
+++ app/src/main/java/com/example/finek/Register.java
@@ -1,7 +1,6 @@
 package com.example.finek;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
